382baa40febcd323af45a1e4de2730748437a6d5
Luis Manuel <luisman40@gmail.com>
2020-10-14 18:03:09 +0200

    Update parser-opts.js

--- packages/conventional-changelog-angular/parser-opts.js
+++ packages/conventional-changelog-angular/parser-opts.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  headerPattern: /^(\w*)(?:\((.*)\))?: (.*)$/,
+  headerPattern: /^(?>Merged PR \d+: )?(\w*)(?:\((.*)\))?: (.*)$/,
   headerCorrespondence: [
     'type',
     'scope',
